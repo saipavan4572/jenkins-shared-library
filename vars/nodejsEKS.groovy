c4bf5b1d5aa6c0c7adb9d4502f6b2fa1150daa8b
saipavan4572 <saipavan4572@gmail.com>
2024-12-31 19:27:23 +0530

    jenkins shared libraries

--- vars/nodejsEKS.groovy
+++ vars/nodejsEKS.groovy
@@ -76,35 +76,35 @@ def call(Map configMap){    // call() will work like main() method in java
             }
             stage('Deploy'){
                 steps{
-                    sh """
-                                 aws eks update-kubeconfig --region ${region} --name ${project}-dev
-                                 cd helm
-                                 sed -i 's/IMAGE_VERSION/${appVersion}/g' values.yaml
-                                 helm uninstall ${component} -n ${project}
-                             """
+                        //   sh """
+                        //          aws eks update-kubeconfig --region ${region} --name ${project}-dev
+                        //          cd helm
+                        //          sed -i 's/IMAGE_VERSION/${appVersion}/g' values.yaml
+                        //          helm uninstall ${component} -n ${project}
+                        //      """
                     
-                    // script{
-                    //      releaseExists = sh(script: "helm list -A --short | grep -w ${component} || true", returnStdout: true).trim()
-                    //     if(releaseExists.isEmpty()){
-                    //         echo "${component} not installed yet, first time installation"
-                    //         sh """
-                    //             aws eks update-kubeconfig --region ${region} --name ${project}-dev
-                    //             cd helm
-                    //             sed -i 's/IMAGE_VERSION/${appVersion}/g' values.yaml
-                    //             helm install ${component} -n ${project} .
-                    //         """
-                    //     }
-                    //     else{
-                    //         echo "${component} exists, running upgrade"
-                    //         sh """
-                    //             aws eks update-kubeconfig --region ${region} --name ${project}-dev
-                    //             cd helm
-                    //             sed -i 's/IMAGE_VERSION/${appVersion}/g' values.yaml
-                    //             helm upgrade ${component} -n ${project} .
-                    //         """
+                    script{
+                         releaseExists = sh(script: "helm list -A --short | grep -w ${component} || true", returnStdout: true).trim()
+                        if(releaseExists.isEmpty()){
+                            echo "${component} not installed yet, first time installation"
+                            sh """
+                                aws eks update-kubeconfig --region ${region} --name ${project}-dev
+                                cd helm
+                                sed -i 's/IMAGE_VERSION/${appVersion}/g' values.yaml
+                                helm install ${component} -n ${project} .
+                            """
+                        }
+                        else{
+                            echo "${component} exists, running upgrade"
+                            sh """
+                                aws eks update-kubeconfig --region ${region} --name ${project}-dev
+                                cd helm
+                                sed -i 's/IMAGE_VERSION/${appVersion}/g' values.yaml
+                                helm upgrade ${component} -n ${project} .
+                            """
 
-                    //     }
-                    // }
+                        }
+                    }
                 }
                 // sed -i 's/IMAGE_VERSION/${appVersion}/g' values.yaml ---> it will replace "IMAGE_VERSION" string with the value in {appVersion} inside values.yaml file.
                 // helm install backend . ---> use for the 1st time installation
